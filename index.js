e5e20cba6f024d7399be7695d282f2b08f1d3492
Ben Mwangi <benmwangi123@gmail.com>
2024-09-08 11:37:06 +1200

    tidy up

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 import { menuArray } from "./data.js";
 
-const itemsList = document.getElementById("item-list");
-const payButton = document.getElementById("pay-btn");
-
 function getMenuHTML() {
   let menuHTML = "";
   menuArray.forEach(function (item) {
-    menuHTML += 
-
     // copy in the div for the menu from html
   });
   return menuHTML;
@@ -17,13 +12,5 @@ console.log(getMenuHTML);
 function showMenu() {
   itemsList.innerHTML = getMenuHTML;
 }
-
-payButton.addEventListener("click", showMenu);
-{
-  console.log("this button isclcickdm");
-}
-// // This is going to be the render items function that wil
-// // change the innerhtml of the shop-item-line div. remember to add
-// the item values as template strings linking to the data.js file object
-// <div class="shop-item-line">
-//
+// having 2 funcitons makes it more efficient because the DOM only has
+// to be manipulated once and not after each iteration
